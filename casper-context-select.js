0060f4c7e2b7893c1af7af4b9389eaab94d746d0
AndreSilva1993 <andreemanuel.1993@gmail.com>
2020-05-06 13:20:29 +0100

    Updates all dependencies to the newly created scope

--- casper-context-select.js
+++ casper-context-select.js
@@ -18,7 +18,7 @@
   -
  */
 
-import '@casper2020/casper-common-ui/casper-overlay.js';
+import '@cloudware-casper/casper-common-ui/casper-overlay.js';
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 
 class CasperContextSelect extends PolymerElement {
--- casper-menu-item.js
+++ casper-menu-item.js
@@ -18,7 +18,7 @@
   -
  */
 
-import '@casper2020/casper-icons/casper-icon.js';
+import '@cloudware-casper/casper-icons/casper-icon.js';
 import '@polymer/iron-icon/iron-icon.js';
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 import { afterNextRender } from '@polymer/polymer/lib/utils/render-status.js';
