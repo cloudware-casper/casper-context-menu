f9dd15bf4c6ae098fe319807e48057ac83ee22b2
Eurico Inocencio <eurico.inocencio@gmail.com>
2021-09-09 10:53:02 +0100

    moving to new casper-icons 3.0 and @lit

--- casper-menu-item.js
+++ casper-menu-item.js
@@ -19,7 +19,6 @@
  */
 
 import '@cloudware-casper/casper-icons/casper-icon.js';
-import '@polymer/iron-icon/iron-icon.js';
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 import { afterNextRender } from '@polymer/polymer/lib/utils/render-status.js';
 
